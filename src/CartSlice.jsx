11c90966b03ae0c7559a14809917c9e81c4d2770
fronceeee <tharathip.tamwiset@gmail.com>
2026-02-15 23:54:52 +0700

    refactor CartSlice to improve code readability and maintainability

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -1,19 +1,35 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 export const CartSlice = createSlice({
-  name: 'cart',
+  name: "cart",
   initialState: {
     items: [], // Initialize items as an empty array
   },
   reducers: {
     addItem: (state, action) => {
-    
+      const newItem = action.payload;
+      const existingItem = state.items.find(
+        (item) => item.name === newItem.name,
+      );
+      if (existingItem) {
+        existingItem.quantity += 1; // Increment quantity if item already exists
+      } else {
+        state.items.push({ ...newItem, quantity: 1 }); // Add new item with quantity 1
+      }
     },
     removeItem: (state, action) => {
+      const itemNameToRemove = action.payload;
+      state.items = state.items.filter(
+        (item) => item.name !== itemNameToRemove,
+      );
     },
     updateQuantity: (state, action) => {
+      const { name, quantity } = action.payload;
+      const itemToUpdate = state.items.find((product) => name === product.name);
 
-    
+      if (itemToUpdate) {
+        itemToUpdate.quantity = quantity;
+      }
     },
   },
 });
